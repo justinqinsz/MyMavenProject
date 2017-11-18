902e2ae61a1babc5980a6d0e2ecc3ae22e4fea09
justin <250578684@qq.com>
2017-11-18 14:41:37 +0800

    artifact direction

---
+++ src/main/java/maven/org/MyMavenProject/App2.java
@@ -0,0 +1,13 @@
+package maven.org.MyMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App2 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "test for artifact direction" );
+    }
+}
