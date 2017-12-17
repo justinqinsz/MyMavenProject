0de6083b481fdec6d6721005fd0555f1923f9e8c
justin <250578684@qq.com>
2017-12-17 23:32:40 +0800

    auto build

--- src/main/java/maven/org/MyMavenProject/App.java
+++ src/main/java/maven/org/MyMavenProject/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("Auto trigger build!");
+        System.out.println("Auto trigger build222");
     }
 }
